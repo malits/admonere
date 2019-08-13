e9c54c6b4b43c4ce33affc9e83d55b71815eea53
malits <maxlitster@gmail.com>
2019-08-13 14:57:19 -0400

    postback handling / completing facebook tutorial

--- index.js
+++ index.js
@@ -127,7 +127,20 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set response based on postback payload
+    if (payload === "yes") {
+        response = {"text": "Thanks!"}
+    } else if (payload == "no") {
+        response = {"text": "Oops, try sending another image"}
+    }
+
+    // Send message back
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response method via the Send API
