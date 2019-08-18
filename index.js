678f776d46267b87d94c83e50308eebf1200aa14
malits <maxlitster@gmail.com>
2019-08-17 22:24:08 -0400

    SQL syntax fix

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.get('/webhook', (req, res) => {
 function databaseTest() {
     client.connect();
 
-    client.query('SELECT table_schema, table_name, FROM information_schema.tables;', (err, res) => {
+    client.query('SELECT table_schema, table_name FROM information_schema.tables;', (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
             console.log(JSON.stringify(row));
